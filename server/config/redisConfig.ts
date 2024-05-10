44fb92b6e008dac21368581e5a7bd21729103cc1
canzeyn <zeyn1616@outlook.com>
2024-05-10 14:53:03 +0300

    kubernjet kuruldu podlar ve servicesler ayarlandı ama hata var redis ve rabbitmq sunucu bağlanılamadı

--- server/config/redisConfig.ts
+++ server/config/redisConfig.ts
@@ -6,7 +6,7 @@ require('dotenv').config();
 // Redis client'ını yapılandır: Redis sunucusuna bağlantı kurmak için bir istemci oluşturur.
 // 'url' ile Redis sunucusunun adresini ve portunu belirtiriz.
 const client = createClient({ 
-    url: `redis://redis:6379` 
+    url: `redis://taskmaster-redis-service:6379` 
 });
 
 // Redis ile iletişim sırasında herhangi bir hata oluşursa bu hatayı yakalar ve konsola yazdırır.
